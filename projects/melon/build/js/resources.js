de85fd6650e6a54ca1ecc71271cb28721607ab19
Chad Eddington <edd11001@byui.edu>
2016-08-17 14:56:26 -0600

    Add built resource file

---
+++ projects/melon/build/js/resources.js
@@ -0,0 +1,27 @@
+game.resources = [
+    {
+        "name": "area01_bkg0",
+        "type": "image",
+        "src": "data/img/area01_bkg0.png"
+    },
+    {
+        "name": "area01_bkg1",
+        "type": "image",
+        "src": "data/img/area01_bkg1.png"
+    },
+    {
+        "name": "area01_level_tiles",
+        "type": "image",
+        "src": "data/img/area01_level_tiles.png"
+    },
+    {
+        "name": "gripe_run_right",
+        "type": "image",
+        "src": "data/img/gripe_run_right.png"
+    },
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.json"
+    }
+];
