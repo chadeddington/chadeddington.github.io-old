139de8850a0ea9d2af78c79496bf4b107c5beafa
Chad Eddington <edd11001@byui.edu>
2017-06-26 08:13:04 -0600

    Add phaser sample

---
+++ projects/melon/data/map/zelda overworld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="zelda copy" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../../../../../Downloads/zelda copy.png" width="512" height="512"/>
+</tileset>
